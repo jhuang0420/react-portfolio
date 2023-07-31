c0de8549916be4900db39ead3ff7dcd4f6f505d0
Jeffrey Huang <jhuang0420@gmail.com>
2023-07-31 17:36:14 -0400

    basic hompage structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../styles/Home.css';
+import '../assests/pegin.jpeg';
+
 const Home = () => {
   return (
-    <div>
-      <></>
+    <div className='home-container'> 
+      <div id='small-box'> HI </div>
+      <div id='small-box'> Hey </div>
+      <div id='small-box'> Hey </div>
+      <div id='long-box'> Hi </div>
+      <div id='long-box'> Hi </div>
+      <div id='long-box'> Hi </div>
+      <div id='small-box'> Hey </div>
     </div>
   );
 };
